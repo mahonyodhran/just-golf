a7c49850a514f375a71e0ceeae28bf44cb2c1149
Odhran OMahony <mahonyodhran@gmail.com>
2023-10-18 22:25:58 +0100

    changed tee choices to be 4 values rather than 2 wrong ones - added method to check if colour already exists

--- courses/models.py
+++ courses/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 TEE_CHOICES = [
-    ("Blue", "White."),
-    ("Green", "Red."),
+    ("Blue", "Blue"),
+    ("White", "White"),
+    ("Green", "Green"),
+    ("Red", "Red"),
 ]
 
 class Course(models.Model):
@@ -20,10 +22,16 @@ class Course(models.Model):
 class Tee (models.Model):
     id = models.AutoField(primary_key=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    colour = models.CharField(choices=TEE_CHOICES, max_length=5)
+    colour = models.CharField(choices=TEE_CHOICES, max_length=10)
     slope = models.IntegerField()
     par = models.IntegerField()
     yards = models.IntegerField()
+    
+    objects = models.Manager()
+
+    def is_colour_available(self):
+        existing_colours = Tee.objects.filter(course=self.course).values_list('colour', flat=True)
+        return self.colour not in existing_colours
 
     class Meta:
         db_table = "tee"
