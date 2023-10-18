f4b6596b22712fd14e6b2b2a104529d07adddc60
Odhran OMahony <mahonyodhran@gmail.com>
2023-10-18 22:26:47 +0100

    added views for viewing a specific course and adding a tee

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
-from .forms import CourseForm
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .forms import CourseForm, TeeForm
 from .models import Course
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import DeleteView
@@ -34,3 +34,35 @@ def add_course(request):
         }
 
         return render(request, "courses/add-course.html", context)
+
+def view_course(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    tees = course.tee_set.all()
+
+    context = {
+        'course': course,
+        'tees': tees,
+    }
+
+    return render(request, 'courses/view-course.html', context)
+
+def add_tee(request):
+    if request.method == "POST":
+        form = TeeForm(request.POST)
+        if form.is_valid():
+            tee = form.save(commit=False)
+            if tee.is_colour_available():
+                tee.save()
+                return render(request, "home.html")
+            else:
+                return HttpResponse("<p>The selected color already exists for this course.</p>")
+        else:
+            return HttpResponse("<p>Info is not valid.</p>")
+
+    else:
+        form = TeeForm
+        context = {
+            "form": form,
+        }
+
+        return render(request, "courses/add-tee.html", context)
