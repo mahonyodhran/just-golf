7a2f350e95ed0f06b1fa5cd6f870e0783926b822
Odhran OMahony <mahonyodhran@gmail.com>
2023-10-18 22:26:20 +0100

    added paths for viewing a specific course and adding a tee

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.course_index, name="course-index"),
     path("add-course", views.add_course, name="add-course"),
     path('delete-course/<int:pk>/', DeleteView.as_view(), name='delete-course'),
+    path("view-course/<int:course_id>/", views.view_course, name="view-course"),
+    path("add-tee", views.add_tee, name="add-tee"),
 ]
