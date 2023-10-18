b3af9234eddf8a63db99896552d92cc0cf34c54c
Odhran OMahony <mahonyodhran@gmail.com>
2023-10-18 22:24:57 +0100

    created a TeeForm

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Course
+from .models import Course, Tee
 
 class CourseForm(ModelForm):
     class Meta:
         model = Course
         fields = ["name", "eircode"]
+        
+class TeeForm(ModelForm):
+    class Meta:
+        model = Tee
+        fields = ["course", "colour", "slope", "par", "yards"]
