d15830321e04400cbe1adc264cadc124da509f08
Odhran OMahony <mahonyodhran@gmail.com>
2023-10-31 19:18:01 +0000

    added add-scorecard

--- scorecards/urls.py
+++ scorecards/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import scorecard_index
+from .views import add_scorecard, scorecard_index
 
 urlpatterns = [
     path("", scorecard_index, name="scorecard-index"),
+    path("add-course", add_scorecard, name="add-scorecard"),
 ]
--- scorecards/views.py
+++ scorecards/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Scorecard
+from .forms import ScorecardForm
 
 def scorecard_index(request):
     scorecards = Scorecard.objects.all()
     return render(request, "scorecards/scorecard-index.html", {"scorecards": scorecards})
+
+def add_scorecard(request):
+    if request.method == "POST":
+        form = ScorecardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home.html")
+        else:
+            return HttpResponse("<p>Info is not Valid</p>")
+
+    else:
+        form = ScorecardForm
+        context = {
+            "form": form,
+        }
+
+        return render(request, "scorecards/add-scorecard.html", context)
