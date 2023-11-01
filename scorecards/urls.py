a8e4ee6a48362eefaa81ec42536338dd44137723
Odhran OMahony <mahonyodhran@gmail.com>
2023-11-01 19:00:07 +0000

    added basic view scorecard path

--- scorecards/urls.py
+++ scorecards/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_scorecard, scorecard_index
+from .views import add_scorecard, scorecard_index, view_scorecard
 
 urlpatterns = [
     path("", scorecard_index, name="scorecard-index"),
     path("add-course", add_scorecard, name="add-scorecard"),
+    path("<int:scorecard_id>/", view_scorecard, name="view-scorecard"),
 ]
