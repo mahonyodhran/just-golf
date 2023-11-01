45dd9d6150a40c3bcb5acea470a9fd914ccf42bc
Odhran OMahony <mahonyodhran@gmail.com>
2023-11-01 22:56:27 +0000

    wrote a basic test case to ... test how to test

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Course
 
-# Create your tests here.
+
+class CourseTestCase(TestCase):
+    def setUp(self):
+        Course.objects.create(name="Waterville", eircode="W123")
+        Course.objects.create(name="Shannon", eircode="S123")
+
+    def test_course_setting(self):
+        """Basic Test For Course"""
+        waterville = Course.objects.get(name="Waterville")
+        self.assertEqual(waterville.name, "Waterville")
